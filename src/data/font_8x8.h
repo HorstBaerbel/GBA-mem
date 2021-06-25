420c7a6a615bc6ee73d99f7bbb729267d2299f55
Bim Overbohm <bim.overbohm@googlemail.com>
2021-06-25 21:38:51 +0200

    CGA background colors, psychooo!

--- src/data/font_8x8.h
+++ src/data/font_8x8.h
@@ -7,8 +7,8 @@
 #define FONT_8X8_WIDTH 8 // width of sprites/tiles in pixels
 #define FONT_8X8_HEIGHT 8 // height of sprites/tiles in pixels
 #define FONT_8X8_BYTES_PER_TILE 32 // bytes for one complete sprite/tile
-#define FONT_8X8_DATA_SIZE 760 // size of sprite/tile data in DWORDs
-#define FONT_8X8_NR_OF_TILES 95 // # of sprites/tiles in data
+#define FONT_8X8_DATA_SIZE 768 // size of sprite/tile data in DWORDs
+#define FONT_8X8_NR_OF_TILES 96 // # of sprites/tiles in data
 extern const uint32_t FONT_8X8_DATA[FONT_8X8_DATA_SIZE];
 #define FONT_8X8_PALETTE_LENGTH 2 // # of palette entries per palette
 #define FONT_8X8_PALETTE_SIZE 2 // size of palette data in WORDs
--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,36 @@
 #include <gba_systemcalls.h>
 #include <gba_interrupt.h>
 
+#include <debug/print.h>
 #include <memory/memory.h>
 #include <tiles.h>
-#include <debug/print.h>
+#include <video.h>
 
 #include "./data/font_8x8.h"
 
+// define all functions here, so we can put them into IWRAM
 int main() IWRAM_CODE;
+void printChar(char c, uint16_t x, uint16_t y, uint16_t backColor = 0, uint16_t textColor = 15) IWRAM_CODE;
+void printString(const char *s, uint16_t x, uint16_t y, uint16_t backColor = 0, uint16_t textColor = 15) IWRAM_CODE;
+// --------------------------------------------------------
 
-void printChar(char c, uint16_t x, uint16_t y, uint16_t color = 15)
+void printChar(char c, uint16_t x, uint16_t y, uint16_t backColor, uint16_t textColor)
 {
 	uint16_t tileNo = static_cast<uint16_t>(c) - 32;
 	uint32_t tileIndex = ((y & 255) * 256) + (x & 255);
-	uint16_t *screen = Tiles::SCREEN_BASE_TO_MEM(Tiles::ScreenBase::Base_1000);
-	screen[tileIndex] = tileNo | ((color & 15) << 12);
+	uint16_t *background = Tiles::SCREEN_BASE_TO_MEM(Tiles::ScreenBase::Base_1000);
+	background[tileIndex] = 95 | ((backColor & 15) << 12);
+	uint16_t *text = Tiles::SCREEN_BASE_TO_MEM(Tiles::ScreenBase::Base_2000);
+	text[tileIndex] = tileNo | ((textColor & 15) << 12);
 }
 
-void printString(const char *s, uint16_t x, uint16_t y, uint16_t color = 15)
+void printString(const char *s, uint16_t x, uint16_t y, uint16_t backColor, uint16_t textColor)
 {
 	if (s != nullptr)
 	{
 		while (*s != '\0')
 		{
-			printChar(*s, x++, y, color);
+			printChar(*s, x++, y, backColor, textColor);
 			s++;
 		}
 	}
@@ -35,12 +42,13 @@ void printString(const char *s, uint16_t x, uint16_t y, uint16_t color = 15)
 int main()
 {
 	// Set graphics to mode 0 and enable background 2
-	REG_DISPCNT = MODE_0 | BG2_ON;
+	REG_DISPCNT = MODE_0 | BG0_ON | BG1_ON;
 	// copy data to tile map
 	Memory::memcpy32(Tiles::TILE_BASE_TO_MEM(Tiles::TileBase::Base_0000), FONT_8X8_DATA, FONT_8X8_DATA_SIZE);
-	// set up background 2 screen and tile map starts and sreen size to 256x256
-	REG_BG2CNT = Tiles::backgroundConfig(Tiles::TileBase::Base_0000, Tiles::ScreenBase::Base_1000);
-	// build palettes - CGA colors, yesss!!!
+	// set up background 0 (text background) and 1 (text foreground) screen and tile map starts and set screen size to 256x256
+	REG_BG0CNT = Tiles::backgroundConfig(Tiles::TileBase::Base_0000, Tiles::ScreenBase::Base_1000, Tiles::ScreenSize::Size_0, 16, 1);
+	REG_BG1CNT = Tiles::backgroundConfig(Tiles::TileBase::Base_0000, Tiles::ScreenBase::Base_2000, Tiles::ScreenSize::Size_0, 16, 0);
+	// build CGA color palette, yesss!!!
 	static const color16 COLORS[16] = {0, 0x5000, 0x280, 0x5280, 0x0014, 0x5014, 0x0154, 0x5294, 0x294a, 0x7d4a, 0x2bea, 0x7fea, 0x295f, 0x7d5f, 0x2bff, 0x7fff};
 	for (uint32_t i = 0; i < 16; i++)
 	{
@@ -50,6 +58,8 @@ int main()
 	uint16_t color = 0;
 	do
 	{
-		printString("Hello world!", 0, 0, color++);
+		printString("Hello world!", 0, 0, color, ~color);
+		color++;
+		Video::waitForVblank(true);
 	} while (true);
 }
